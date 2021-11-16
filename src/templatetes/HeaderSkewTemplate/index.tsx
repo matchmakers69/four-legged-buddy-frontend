ed8ec7e029597792cf32e9030b1355bbad679196
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-11-16 13:58:55 +0000

    Fixed some errors

--- src/templatetes/HeaderSkewTemplate/index.tsx
+++ src/templatetes/HeaderSkewTemplate/index.tsx
@@ -1,11 +1,11 @@
-import { FC, ReactNode } from "react";
+import { VFC, ReactNode } from "react";
 import * as S from "./HeaderSkewTemplate.styled";
 
 interface IProps {
   children: ReactNode;
 }
 
-const HeaderSkewTemplate: FC<IProps> = ({ children }) => {
+const HeaderSkewTemplate: VFC<IProps> = function ({ children }) {
   return (
     <S.HeroSkewHeader className="hero-section-header">
       <S.InnerHeader className="inner-header">
