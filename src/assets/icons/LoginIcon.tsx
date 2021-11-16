84ea438f49f10ead34d30929d364693729026bcd
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-11-16 13:48:09 +0000

    Updated errors

--- src/assets/icons/LoginIcon.tsx
+++ src/assets/icons/LoginIcon.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
+import { IPropsIcons } from "../interface/icons";
 
-const Icon: FC = ({ ...props }) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 13">
-    <path className="a" d="M5.25,0V1.857h7v9.286h-7V13H14V0ZM7,3.714V5.571H0V7.429H7V9.286L10.5,6.5Z" />
-  </svg>
-);
+const Icon: FC<IPropsIcons> = function ({ className }) {
+  return (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 13">
+      <path className="a" d="M5.25,0V1.857h7v9.286h-7V13H14V0ZM7,3.714V5.571H0V7.429H7V9.286L10.5,6.5Z" />
+    </svg>
+  );
+};
 
 export default Icon;
--- src/assets/icons/SpinnerIcon.tsx
+++ src/assets/icons/SpinnerIcon.tsx
@@ -1,18 +1,17 @@
-import { VFC } from "react";
+import { FC } from "react";
+import { IPropsIcons } from "../interface/icons";
 
-type Props = {
-  className: string;
+const Icon: FC<IPropsIcons> = function ({ className }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className={className}>
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+  );
 };
-
-const Icon: VFC<Props> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className={className}>
-    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-    <path
-      className="opacity-75"
-      fill="currentColor"
-      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-    />
-  </svg>
-);
 
 export default Icon;
