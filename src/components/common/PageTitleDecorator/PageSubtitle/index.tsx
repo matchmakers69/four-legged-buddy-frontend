4a4a8218c146380a8e0c5473604fad6134db9f69
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-11-16 13:57:36 +0000

    Fixed some errors

--- src/components/common/PageTitleDecorator/PageSubtitle/index.tsx
+++ src/components/common/PageTitleDecorator/PageSubtitle/index.tsx
@@ -1,11 +1,11 @@
-import { FC } from "react";
+import { VFC } from "react";
 import * as S from "../common.styled";
 
 interface IProps {
   subTitle: string;
 }
 
-const PageSubtitle: FC<IProps> = ({ subTitle }) => {
+const PageSubtitle: VFC<IProps> = function ({ subTitle }) {
   return <S.SubtitleDecorator>{subTitle}</S.SubtitleDecorator>;
 };
 
