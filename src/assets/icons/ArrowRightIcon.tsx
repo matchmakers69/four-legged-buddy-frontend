e2a549d98b759a62a53bf34f9e23b9f146872091
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2021-11-16 13:43:41 +0000

    Updated errors

--- src/assets/icons/ArrowRightIcon.tsx
+++ src/assets/icons/ArrowRightIcon.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
+import { IPropsIcons } from "../interface/icons";
 
-const Icon: FC = ({ ...props }) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" />
-  </svg>
-);
+const Icon: FC<IPropsIcons> = function ({ className }) {
+  return (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" />
+    </svg>
+  );
+};
 
 export default Icon;
