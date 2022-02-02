c890bce7e32fb772d01f7d201db32699cc0bb276
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-02 23:18:57 +0000

    Updated [slug].tsx - removed Edit and Delete buttons

--- src/pages/members/[slug].tsx
+++ src/pages/members/[slug].tsx
@@ -1,13 +1,8 @@
 /* eslint-disable no-restricted-globals */
 import { ParsedUrlQuery } from "querystring";
 import { VFC } from "react";
-import axios from "axios";
 import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
-import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AppLink from "src/components/common/AppLink";
-import { Button } from "src/components/common/Button/Button.styled";
 import Layout from "src/components/Layout";
 import { API_URL } from "src/config";
 import constants from "src/constants";
@@ -28,20 +23,6 @@ interface IParams extends ParsedUrlQuery {
 }
 
 const Member: VFC<IMemberProps> = function ({ member }) {
-  const router = useRouter();
-  const onDeleteMember = async () => {
-    try {
-      const res = await axios.delete(`${API_URL}/members/${member?.id}`);
-      if (res.status === 200) {
-        router.push("/members");
-      } else {
-        toast.error("Something went wrong!");
-      }
-    } catch (err) {
-      console.log(err);
-      toast.error("Something went wrong!");
-    }
-  };
   return (
     <Layout pageTitle="Member">
       <GridTemplate>
@@ -49,17 +30,8 @@ const Member: VFC<IMemberProps> = function ({ member }) {
           <H1 className="h1 bold m-30-bottom">
             <span className="title-paragraph">{member ? member?.name : "Member does not exists"}</span>
           </H1>
-
-          <Button onClick={onDeleteMember} className="btn--danger" type="button">
-            Remove member
-          </Button>
-
-          <AppLink variant="link" href={`/members/edit/${member?.id}`}>
-            Edit
-          </AppLink>
         </Col>
       </GridTemplate>
-      <ToastContainer />
     </Layout>
   );
 };
