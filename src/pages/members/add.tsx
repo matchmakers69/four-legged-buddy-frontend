2efb223b7526e85f90f0849fe81848a7edcb7a83
Przemek Lewtak <przemek.lewtak@orcha.co.uk>
2022-02-02 21:56:43 +0000

    Updated add.tsx added Bearer token from props

--- src/pages/members/add.tsx
+++ src/pages/members/add.tsx
@@ -7,7 +7,12 @@ import { Col } from "src/styles/grid";
 import { H1 } from "src/styles/typography";
 import GridTemplate from "src/templatetes/GridTemplate";
 
-const AddMember: FC = function () {
+type IAddMemberProps = {
+  isCookieToken: string;
+  userToken: string;
+};
+
+const AddMember = function ({ isCookieToken, userToken }: IAddMemberProps) {
   return (
     <Layout pageTitle="Members - Add member">
       <GridTemplate>
@@ -15,16 +20,16 @@ const AddMember: FC = function () {
           <H1 className="h1 bold m-30-bottom">
             <span className="title-paragraph">Add member</span>
           </H1>
-          <AddMemberForm />
+          <AddMemberForm token={isCookieToken} userToken={userToken} />
         </Col>
       </GridTemplate>
     </Layout>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = withProtectedRoute(() => {
+export const getServerSideProps: GetServerSideProps = withProtectedRoute(async ({ isCookieToken, userToken }) => {
   return {
-    props: {},
+    props: { isCookieToken, userToken },
   };
 });
 
